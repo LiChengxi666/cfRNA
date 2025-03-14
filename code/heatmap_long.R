b96b5f4c1c73f52a9df216a585393eb8814a5056
lichengxi <2039276961@qq.com>
2025-03-14 08:56:58 +0800

    添加新的 R 数据文件和历史记录，更新 heatmap_long.R 中的注释设置和绘图参数

--- code/heatmap_long.R
+++ code/heatmap_long.R
@@ -7,19 +7,18 @@ matadata <- read.table("../matrix/oaps.metadata.txt", sep='\t', header = TRUE, r
 count_matrix <- t(count_data)
 
 # 设置注释
-annotation <- matadata$label
-annotation[annotation != "NC"] <- "cancer"
+annotation <- matadata$Condition
 annotation_col = data.frame(Type = factor(annotation))
 rownames(annotation_col) = colnames(count_matrix)
-ann_colors = list(Type = c("cancer"="brown3", "NC"="darkgreen"))
+ann_colors = list(Type = c("OAPS"="brown3", "Non-OAPS"="darkgreen"))
 bk <- c(seq(-2,-0.1,by=0.01),seq(0,2,by=0.01))
 
 # 绘图并保存
 pheatmap(count_matrix,
          clustering_method = "ward.D2",
          cutree_row = 2, cutree_col = 2, 
-         cluster_rows=TRUE, show_rownames = TRUE, 
-         cluster_cols=FALSE, show_colnames = FALSE,
+         cluster_rows=TRUE, show_rownames = FALSE, 
+         cluster_cols=TRUE, show_colnames = FALSE,
          annotation_col = annotation_col, annotation_colors = ann_colors,
          scale = "none",
          color = c(colorRampPalette(colors = c("blue","white"))(length(bk)/2),
